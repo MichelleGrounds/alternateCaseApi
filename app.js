80f97fc95fced6a1dea162d210d8d7d8c641e83f
Michelle Grounds <mischoel@googlemail.com>
2019-11-26 20:44:57 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const alternateCases = require("./case-controller");
+app.use(express.json());
+
+app.get("/api", alternateCases);
+
+module.exports = app;
