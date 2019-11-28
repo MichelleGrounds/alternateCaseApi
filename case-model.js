08c1b5b8c28bbb7bc412abd2e5dc8f5f5356b7e5
Michelle Grounds <mischoel@googlemail.com>
2019-11-28 23:01:33 +0000

    mystery

--- case-model.js
+++ case-model.js
@@ -1,7 +1,6 @@
 exports.getCaseData = (lowercase, uppercase) => {
   let result;
 
-  const switchFunction = () => {
   switch (lowercase) {
     case undefined:
       break;
@@ -14,9 +13,13 @@ exports.getCaseData = (lowercase, uppercase) => {
     default:
       result = casusLitterisAlternatim[uppercase];
   }
-  };
 
-  setTimeout(switchFunction, 1000);
+  function fibonacci(n) {
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+  fibonacci(10);
 
   return Promise.resolve(result);
 };
